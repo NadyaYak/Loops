05c358124c74b8e187f89e7c3be49c303eeadb39
NadyaYak <122393665+NadyaYak@users.noreply.github.com>
2023-02-27 03:34:18 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+// for Loops
+
+// for(i = 10; i >= 0;i--){
+//   console.log(i)
+// }
+
+// -------------------
+// let students = ["Riley", "Isaac", "Zen", "Boaz", "Shani", "Nadyha"]
+
+
+// for (i = 0; i < students.length; i++) {
+//   console.log(`i is now ${i} and the current student is ${students[i]}`)
+// }
+// // -------------------
+
+// for (y = 0; y < 100; y++) {
+//   if (y % 2 === 0) {
+//     console.log(y, "even")
+//   } else {
+//     console.log(y, "odd")
+//   }
+// }
+// // -------------------
+
+
+// While Loop
+
+// let age = 0
+// while(age < 30){
+//   console.log(age,'Happy Birthday')
+//   age++
+//   if(age === 16){
+//     console.log("Sweet 16!")
+//   }
+//   if(age === 18){
+//     console.log("You Grown!")
+//   }
+//   if(age === 21){
+//     console.log("Yay! You can go to a bar!")
+//   }
+//   else if(age >= 22){
+//       console.log('Nobody Cares')
+//   }
+// }
+// // // -------------------
